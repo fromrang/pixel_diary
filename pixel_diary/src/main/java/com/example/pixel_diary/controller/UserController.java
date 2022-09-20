59ccf093daa88e342ffe6ec9b839d76489540909
fromrang <99739254+fromrang@users.noreply.github.com>
2022-09-20 18:06:28 +0900

    Add files via upload

--- pixel_diary/src/main/java/com/example/pixel_diary/controller/UserController.java
+++ pixel_diary/src/main/java/com/example/pixel_diary/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.example.pixel_diary.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.pixel_diary.dto.Message;
@@ -8,14 +10,16 @@ import com.example.pixel_diary.dto.User;
 
 @RestController
 public class UserController {
-	@GetMapping("/join")
-	public Message joinAction(){
-		Message message = null;
-		//System.out.println(user.getId());
-//		if(user.getId() == null) {
-//			message = new Message(400, "FAIL", "join error");
-//		}
-		message = new Message(200, "SUCCESS", "join");
-		return message;
+	@Autowired
+	User user;
+	@PostMapping("/user")
+	public Message join(){
+
+		return null;
+	}
+
+	@GetMapping("/user")
+	public User login(){
+		return user;
 	}
 }
