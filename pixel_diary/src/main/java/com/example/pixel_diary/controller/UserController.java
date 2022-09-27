00c84feb09f85613a00579d271e269b62fd0fa09
fromrang <99739254+fromrang@users.noreply.github.com>
2022-09-27 18:35:43 +0900

    Add files via upload

--- pixel_diary/src/main/java/com/example/pixel_diary/controller/UserController.java
+++ pixel_diary/src/main/java/com/example/pixel_diary/controller/UserController.java
@@ -1,21 +1,17 @@
 package com.example.pixel_diary.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
-
 import com.example.pixel_diary.dao.UserDao;
 import com.example.pixel_diary.dto.DefaultRes;
 import com.example.pixel_diary.dto.StatusCode;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import com.example.pixel_diary.dto.User;
 
 @RestController
 public class UserController {
-	
 	@Autowired
 	UserDao userDao;
 
@@ -24,11 +20,13 @@ public class UserController {
 	public ResponseEntity join(@RequestBody User user){
 		try {
 			User newUser = userDao.getUser(user.getId());
+			//System.out.println("!!! "+user.getId());
 			if (newUser != null) {
+				//System.out.println(newUser.getId());
 				return new ResponseEntity(DefaultRes.res(StatusCode.CREATED, "[Fail]exist user"), HttpStatus.OK);
 			}else{
 				userDao.insertUser(user);
-				return new ResponseEntity(DefaultRes.res(StatusCode.OK, "[SUCCESS]join", user), HttpStatus.OK);
+				return new ResponseEntity(DefaultRes.res(StatusCode.OK, "[SUCCESS]join", newUser), HttpStatus.OK);
 			}
 		}catch(Exception e){
 			e.printStackTrace();
@@ -38,8 +36,19 @@ public class UserController {
 	}
 	//로그인
 	@GetMapping("/user")
-	public User login(){
-		return null;
+	public ResponseEntity login(@RequestBody User user){
+		try{
+			User existUser = userDao.getUserIdPw(user.getId(), user.getPassword());
+			if(existUser == null){
+				return new ResponseEntity(DefaultRes.res(StatusCode.CREATED, "[Fail]not exist user"), HttpStatus.OK);
+			}else{
+				return new ResponseEntity(DefaultRes.res(StatusCode.OK, "[SUCCESS]login", existUser), HttpStatus.OK);
+			}
+		}catch (Exception e){
+			e.printStackTrace();
+			return new ResponseEntity(HttpStatus.BAD_REQUEST);
+
+		}
 	}
 	//회원 수정
 	//@PutMapping("/user")
@@ -54,18 +63,21 @@ public class UserController {
 			@RequestParam(value = "msg2", required = false, defaultValue = "msg2Default")String msg2) {
 		Hello hello = new Hello(0, msg);
 		hello.setMsg(hello.getMsg() + ", " + msg2);
+
 		return hello;
 	}
+
 	@PostMapping()
 	public Hello post(@RequestBody Param param) {
 		String msg = "Data1 : " + param.getData1() + ", Data2 : " + param.getData2();
 		Hello hello = new Hello(0, msg);
 		return hello;
 	}
+
 	@PutMapping()
 	public Hello put(@RequestParam(value = "param1")String param1) {
 		Hello hello = new Hello(0, "Put Request : " + param1);
 		return hello;
 	}
-	*/ 
+	*/
 }
