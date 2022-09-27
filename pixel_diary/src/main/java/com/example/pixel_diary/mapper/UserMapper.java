418774429e7e3286a15a5ab998e32279a7c93ac5
fromrang <99739254+fromrang@users.noreply.github.com>
2022-09-27 18:36:56 +0900

    Add files via upload

--- pixel_diary/src/main/java/com/example/pixel_diary/mapper/UserMapper.java
+++ pixel_diary/src/main/java/com/example/pixel_diary/mapper/UserMapper.java
@@ -15,4 +15,7 @@ public interface UserMapper {
     @Insert("insert into user(id, password, nickname, birthDay) values(#{id}, #{password}, #{nickname}, #{birthDay})")
     public void insertUser(User user) throws Exception;
 
+    @Select("select * from user where id=#{id} and password=#{password}")
+    public User selectUserIdPw(@Param("id")String id, @Param("password")String password) throws Exception;
+
 }
